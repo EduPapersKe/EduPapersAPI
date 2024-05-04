c11498306bec626fb23a3681b22acb0476c099c0
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-05-04 03:33:36 +0300

    Fix: Reconfigured url routes correctly

--- users/urls.py
+++ users/urls.py
@@ -10,9 +10,9 @@ from .views import UserViewset, LoginView, LogoutView
 urlpatterns = [
     path('users/', UserViewset.as_view({'post': 'register'}), name='create-user'),
     path('users/<uuid:pk>/', UserViewset.as_view({'get': 'retrieve'}), name='get-user'),
-    path('users/<uuid:pk>/', UserViewset.as_view({'put': 'update'}), name='update-user'),
-    path('users/<uuid:pk>/', UserViewset.as_view({'patch': 'partial_update'}), name='update-user-partially'),
-    path('users/<uuid:pk>/', UserViewset.as_view({'delete': 'destroy'}), name='delete-user'),
+    path('users/<uuid:pk>/update/', UserViewset.as_view({'put': 'update'}), name='update-user'),
+    path('users/<uuid:pk>/patch/', UserViewset.as_view({'patch': 'partial_update'}), name='update-user-partially'),
+    path('users/<uuid:pk>/delete/', UserViewset.as_view({'delete': 'destroy'}), name='delete-user'),
     path('users/login/', LoginView.as_view(), name='login'),
     path('users/logout/', LogoutView.as_view(), name='log-out'),
 ]
