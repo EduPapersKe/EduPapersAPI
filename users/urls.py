b34f41c36ae963901c52ecfe66e01b4ea92184e2
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-05-04 03:12:44 +0300

    Fix: Fixed an issue with the custom definition of urls for each function

--- users/urls.py
+++ users/urls.py
@@ -1,15 +1,20 @@
-from rest_framework.routers import SimpleRouter
+# from rest_framework.routers import SimpleRouter
 from django.urls import path
 
 from .views import UserViewset, LoginView, LogoutView
 
-router = SimpleRouter()
-router.register(r'users', UserViewset)
+# router = SimpleRouter()
+# router.register(r'users', UserViewset)
 
 
 urlpatterns = [
+    path('users/', UserViewset.as_view({'post': 'register'}), name='create-user'),
+    path('users/<uuid:pk>/', UserViewset.as_view({'get': 'retrieve'}), name='get-user'),
+    path('users/<uuid:pk>/', UserViewset.as_view({'put': 'update'}), name='update-user'),
+    path('users/<uuid:pk>/', UserViewset.as_view({'patch': 'partial_update'}), name='update-user-partially'),
+    path('users/<uuid:pk>/', UserViewset.as_view({'delete': 'destroy'}), name='delete-user'),
     path('users/login/', LoginView.as_view(), name='login'),
     path('users/logout/', LogoutView.as_view(), name='log-out'),
 ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
