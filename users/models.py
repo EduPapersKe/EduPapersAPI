b706ae54822e989fc0b4450d951bc73d5ae6a89d
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-05-04 03:33:06 +0300

    Fix: Added the field 'is_staff' in User

--- users/models.py
+++ users/models.py
@@ -42,6 +42,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_verified = models.BooleanField(default=False)
     
     is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
     is_admin = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     
