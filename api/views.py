58d0fb0608952b36fad35102a4bd70f7838eef3a
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-05-07 09:46:40 +0300

    Fix: Allow unauthenticated users to access resources
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>

--- api/views.py
+++ api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .models import Resource
 from .serializers import ResourceSerializer
@@ -23,7 +23,13 @@ def format_file_size(file_size_bytes):
 class ResourceView(viewsets.ModelViewSet):
     queryset = Resource.objects.all()
     serializer_class = ResourceSerializer
-    permission_classes = [IsAuthenticated]
+    
+    def get_permissions(self):
+        if self.action in ['post', 'update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
     
     @action(methods=['GET'], detail=False)
     def get(self, request):
