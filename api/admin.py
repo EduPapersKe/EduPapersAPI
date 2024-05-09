d8bdce86ff43d78e5daa0a56996d6d441cdcdf2a
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-05-09 22:09:45 +0300

    Registered models in admin panel
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>

--- api/admin.py
+++ api/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Resource, Tag
+from .models import Resource, Tag, Comment, Category
 
 admin.site.register(Resource)
 admin.site.register(Tag)
-# admin.site.register()
-# admin.site.register()
+admin.site.register(Comment)
+admin.site.register(Category)
