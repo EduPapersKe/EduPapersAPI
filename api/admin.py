05fa444b48adc47dc6efee561a82e03fa5343922
mystro ideas <85731257+bedah-kym@users.noreply.github.com>
2024-05-21 16:28:15 +0300

    Bedah kym patch 1 (#64)
    * DB config
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Docker deployment config (#41)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * delete contributing.md (#42)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Hid migration files (#43)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Gitignore (#44)
    
    * Hid migration files
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Delete migration in development
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    ---------
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Implemented CRUD on resources (#46)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Corrected database settings (#49)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Fix: Allow unauthenticated users to access resources (#50)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Update README (#53)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Created a model to store Tag categories. (#55)
    
    * Update README
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Created a model for comments
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Created models for tags and their categories
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Registered models in admin panel
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    ---------
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Created model for comments (#54)
    
    * Update README
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Created a model for comments
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    ---------
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * comment limit feature
    
    * Return string (#61)
    
    * DB config (#40)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Delete migrations (#45)
    
    * DB config
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Docker deployment config (#41)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * delete contributing.md (#42)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Hid migration files (#43)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Gitignore (#44)
    
    * Hid migration files
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Delete migration in development
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    ---------
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    ---------
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Fix: remove f-string
    
    Signed-off-by: aghee <agymunzala@gmail.com>
    
    ---------
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    Signed-off-by: aghee <agymunzala@gmail.com>
    Co-authored-by: Brian Mwangi <127291274+mwangi-brian@users.noreply.github.com>
    
    * Resolve return str error (#62)
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    
    * Update models.py
    
    added comment limit
    
    ---------
    
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    Signed-off-by: aghee <agymunzala@gmail.com>
    Co-authored-by: Brian Mwangi <brianmwangikamau4@gmail.com>
    Co-authored-by: Brian Mwangi <127291274+mwangi-brian@users.noreply.github.com>
    Co-authored-by: BEDAH <>
    Co-authored-by: Agy M <agymunzala@gmail.com>

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Resource, Tag, Comment, Category
+
+admin.site.register(Resource)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(Category)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+from .models import Resource
+from .serializers import ResourceSerializer
+
+
+#pylint: disable=no-member
+def format_file_size(file_size_bytes):
+    if file_size_bytes < 1024:
+        return f"{file_size_bytes} B"
+    elif file_size_bytes < 1024 * 1024:
+        return f"{file_size_bytes / 1024:.1f} KB"
+    elif file_size_bytes < 1024 * 1024 * 1024:
+        return f"{file_size_bytes / (1024 * 1024):.1f} MB"
+    else:
+        return f"{file_size_bytes / (1024 * 1024 * 1024):.1f} GB"
+        
+        
+class ResourceView(viewsets.ModelViewSet):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+    
+    def get_permissions(self):
+        if self.action in ['post', 'update', 'destroy']:
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
+    
+    @action(methods=['GET'], detail=False)
+    def get(self, request):
+        queryset = self.get_queryset()
+        serializer = ResourceSerializer(queryset, many=True)
+        return Response(serializer.data)
+        
+        
+    @action(methods=['GET'], detail=True)
+    def retrieve(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    @action(methods=['POST'], detail=False)
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        file = request.FILES.get('file')
+        max_size = 100 * 1024 * 1024  # Sets the max size to 100MB
+        allowed_formats = ['PDF', 'DOCX', 'XLSX', 'CSV', 'ODS', 'ZIP', 'TXT', 'EPUB', 'MOBI', 'AZW']
+
+        if file.size > max_size:
+            raise ValidationError({'error': 'File size exceeds the maximum allowed size.'})
+
+        file_format = file.name.split('.')[-1].upper()
+        if file_format not in allowed_formats:
+            raise ValidationError({'error': 'Invalid file format.'})
+
+        self.perform_create(serializer)
+
+        resource = serializer.instance
+        resource.publisher = self.request.user
+        resource.file_size = file.size
+        resource.file_format = file_format
+        resource.save()
+
+        resource.readable_size = format_file_size(file.size)
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
+    @action(methods=['PUT'], detail=True)
+    def update(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        resource = serializer.instance
+        resource.publisher = self.request.user
+        resource.save()
+
+        file = request.FILES.get('file', None)
+        if file:
+            return Response({'error': 'File update is not allowed.'}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    @action(methods=['DELETE'], detail=True)
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
