3bad1e922e43700bff95465e8504f849d850c17f
mystro ideas <85731257+bedah-kym@users.noreply.github.com>
2024-05-21 16:26:48 +0300

    simple dev keys can now be generated (#59)
    Co-authored-by: BEDAH <>

--- root/settings.py
+++ root/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'rest_framework_simplejwt',
     'api',
     'users',
