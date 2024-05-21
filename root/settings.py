5eb7131b679b14c023fb0e1bcc0d8031415dce31
Brian Mwangi <brianmwangikamau4@gmail.com>
2024-05-21 17:48:36 +0300

    feat: Define api key middleware in settings
    Signed-off-by: Brian Mwangi <brianmwangikamau4@gmail.com>

--- root/settings.py
+++ root/settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'api.middleware.ClientMiddleware',
 ]
 
 ROOT_URLCONF = 'root.urls'
