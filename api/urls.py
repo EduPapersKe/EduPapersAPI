b863502c500d885ca6fdc3bd3ac66772bbf93975
BEDAH <>
2024-06-19 17:43:12 +0300

    finished comments

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 # from rest_framework.routers import SimpleRouter
 
-from .views import ResourceView
+from .views import ResourceView,CommentView
 
 # router = SimpleRouter
 
@@ -12,6 +12,12 @@ urlpatterns = [
     path('resources/create/', ResourceView.as_view({'post': 'post'}), name='create resource'),
     path('resources/<uuid:pk>/update/', ResourceView.as_view({'put': 'update'}), name='update resource'),
     path('resources/<uuid:pk>/delete/', ResourceView.as_view({'delete': 'destroy'}), name='delete resource'),
+    path('comments/', CommentView.as_view({'get': 'list'}), name='all-comments'),
+    path('comments/create/', CommentView.as_view({'post':'create'}), name='create-comment'),
+    path('comments/<uuid:pk>/', CommentView.as_view({'get':'retrieve'}), name='get-comment'),
+    path('comments/<uuid:pk>/update/', CommentView.as_view({'put': 'update'}), name='update-comment'),
+    path('comments/<uuid:pk>/delete/', CommentView.as_view({'delete': 'destroy'}), name='delete-comment'),
+    
 ]
 
 # urlpatterns += router.urls
